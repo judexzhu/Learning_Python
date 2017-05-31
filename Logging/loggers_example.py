57f5529fadc8666f6ffeb0cc0d0f902ccd92baad
Jude Zhu <Jude.x.zhu@gmail.com>
2017-05-31 11:01:12 +0800

    Create loggers_example.py

---
+++ Logging/loggers_example.py
@@ -0,0 +1,18 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('sample.log')
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+logger.info ('tests')
